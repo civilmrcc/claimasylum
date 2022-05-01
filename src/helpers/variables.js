861c1d2ff2bd65d07b3e0af21c091ba35c526c41
Nils Kratochwil <n.kratochwil@posteo.de>
2022-05-01 11:01:24 +0200

    frontend

---
+++ src/helpers/variables.js
@@ -0,0 +1,3 @@
+export const API_URL = "http://localhost:5000";
+export const USER_DATA_KEY = "USER_DATA";
+export const ACCESS_DATA_KEY = "ACCESS_DATA";
---
+++ src/hooks/useData.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import React from "react";
+import { API_URL } from "../helpers/variables";
+
+const useData = () => {
+  const sendFormData = async (data) => {
+    try {
+      const response = await axios.post(`${API_URL}/userdata`, data);
+      console.log(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getFormData = async (data) => {
+    try {
+      const response = await axios.get(`${API_URL}/userdata`);
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const postAccessData = async (data) => {
+    try {
+      const response = await axios.post(`${API_URL}/accessdata`, data);
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return {
+    sendFormData,
+    getFormData,
+    postAccessData,
+  };
+};
+
+export default useData;
