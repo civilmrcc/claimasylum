66ec5a6b56a7760bfaa36c3c0d606ac280aabf9a
niczem <ahab@systemli.org>
2022-07-08 01:19:41 +0200

    first poc of pgp key genearation and encryption

--- backend/applications/views.py
+++ backend/applications/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.mail import send_mail
+import pgpy
 
 def index(request):
+    PUBLIC_KEY_FILE = '/code/publickeys/public.asc'    
+    pub_key, _ = pgpy.PGPKey.from_file(str(PUBLIC_KEY_FILE))
+    
+    # Encrypt string
+    txt_msg = pgpy.PGPMessage.new("Hello PGPy World")
+    print('txt_msg.is_encrypted')
+    print(txt_msg.is_encrypted)
+    print('txt_msg.message')
+    print(txt_msg.message)
+    encrypted_txt_msg = pub_key.encrypt(txt_msg)
+    print('encrypted_txt_msg.is_encrypted')
+    print(encrypted_txt_msg.is_encrypted)
+    print('encrypted_txt_msg.message')
+    print(bytes(encrypted_txt_msg))
     print('asdasd')
+    
     send_mail(
         'Subject here',
         'Here is the message.',
