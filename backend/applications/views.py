909d17a276fb5f8329db8f55a44b4d4766761ccc
niczem <ahab@systemli.org>
2022-08-09 00:33:03 +0200

    encryption of request and mail submission implemented

--- backend/applications/views.py
+++ backend/applications/views.py
@@ -1,31 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.mail import send_mail
+
 import pgpy
+import json
+import os
 
 def index(request):
+    #get formdata
+    formdata = json.loads(request.body)['formdata']
+
     PUBLIC_KEY_FILE = '/code/publickeys/public.asc'    
     pub_key, _ = pgpy.PGPKey.from_file(str(PUBLIC_KEY_FILE))
-    
+
     # Encrypt string
-    txt_msg = pgpy.PGPMessage.new("Hello PGPy World")
-    print('txt_msg.is_encrypted')
-    print(txt_msg.is_encrypted)
-    print('txt_msg.message')
-    print(txt_msg.message)
+    txt_msg = pgpy.PGPMessage.new(json.dumps(formdata))
     encrypted_txt_msg = pub_key.encrypt(txt_msg)
-    print('encrypted_txt_msg.is_encrypted')
-    print(encrypted_txt_msg.is_encrypted)
-    print('encrypted_txt_msg.message')
-    print(bytes(encrypted_txt_msg))
-    print('asdasd')
-    
+    #EMAIL_HOST_USER
     send_mail(
         'Subject here',
-        'Here is the message.',
-        'claimasylum-notifications@systemli.org',
-        ['nic.zemke@systemli.org'],
+        str(encrypted_txt_msg),
+        os.environ.get('EMAIL_HOST_USER'),
+        [os.environ.get('EMAIL_HOST_USER')],
         fail_silently=False,
     )
-    print('asdasd')
+    print("!!DEBUG MODE: Mail sent to own mail")
     return HttpResponse(200)
