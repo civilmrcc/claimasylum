92ee00a9b1d3f7863c3815dc66ae70fa858ae52d
niczem <ahab@systemli.org>
2022-08-09 02:32:53 +0200

    feature: messages are also getting signed now

--- backend/applications/views.py
+++ backend/applications/views.py
@@ -10,18 +10,22 @@ def index(request):
     #get formdata
     formdata = json.loads(request.body)['formdata']
 
-    PUBLIC_KEY_FILE = '/code/publickeys/public.asc'    
-    pub_key, _ = pgpy.PGPKey.from_file(str(PUBLIC_KEY_FILE))
-
+    pub_key, _ = pgpy.PGPKey.from_file(str('/code/publickeys/public.asc'))
+    priv_key, _ = pgpy.PGPKey.from_file(str('/code/privatekeys/private.pgp'))
+   
     # Encrypt string
     txt_msg = pgpy.PGPMessage.new(json.dumps(formdata))
     encrypted_txt_msg = pub_key.encrypt(txt_msg)
+    # from pgpy documentation:
+    # the bitwise OR operator '|' is used to add a signature to a PGPMessage.
+    signature = priv_key.sign(encrypted_txt_msg)
+
     #EMAIL_HOST_USER
     send_mail(
         'New Request submitted',
-        str(encrypted_txt_msg),
+        str(encrypted_txt_msg)+'\n'+str(signature),
         getattr(settings, "EMAIL_FROM_MAIL", None),
-        [getattr(settings, "CLAIMASYLUM_NOTIFICATION_MAIL", None)],
+        [getattr(settings, "EMAIL_FROM_MAIL", None)],
         fail_silently=False,
     )
     print("Mail sent.")
