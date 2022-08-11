6d9b497f2f70e75822198f3fee8b275b197c7706
niczem <ahab@systemli.org>
2022-08-11 03:22:06 +0200

    feature: logging implemented in django

--- backend/applications/views.py
+++ backend/applications/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.mail import send_mail
 from django.conf import settings
-
+import logging
 import pgpy
 import json
 
+
+logger = logging.getLogger('claimasylum')
+
 def index(request):
+    logger.info('Form Submitted')
     #get formdata
     formdata = json.loads(request.body)['formdata']
 
@@ -19,7 +23,6 @@ def index(request):
     # from pgpy documentation:
     # the bitwise OR operator '|' is used to add a signature to a PGPMessage.
     encrypted_txt_msg |= priv_key.sign(encrypted_txt_msg)
-
     #EMAIL_HOST_USER
     send_mail(
         'New Request submitted',
@@ -28,5 +31,6 @@ def index(request):
         [getattr(settings, "CLAIMASYLUM_NOTIFICATION_MAIL", None)],
         fail_silently=False,
     )
+    logger.info('Request from server successfull')
     print("Mail sent.")
     return HttpResponse(200)
