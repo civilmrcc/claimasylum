366fc59e455710882fc666921485754f31de8255
niczem <ahab@systemli.org>
2023-07-23 16:25:57 +0200

    distance function added to initial request

--- backend/applications/views.py
+++ backend/applications/views.py
@@ -6,6 +6,7 @@ from django.conf import settings
 
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
+from dataclasses import dataclass
 
 import logging
 import pgpy
@@ -24,19 +25,51 @@ def validate_coordinates(position):
         features = json.load(f)["features"]
 
     point = Point(position)
+    poly_items = []
     matched_polygon = None
-    for area in features:
+    @dataclass
+    class Item:
+        polygon:Polygon
+        area:dict
+        def distance_to_point(self, point:Point):
+            return self.polygon.exterior.distance(point)
+
+    for area_id, area in enumerate(features):
+        print(area['geometry']['type'], area_id)
         if area['geometry']['type'] == 'Polygon':
+            print("got polygon")
             polygon = Polygon(area['geometry']['coordinates'][0])
-            if polygon.contains(point):
+            poly_item  = Item(Polygon(area['geometry']['coordinates'][0]), area)
+            poly_items.append(poly_item)
+            if poly_item.polygon.contains(point):
                 # check if looped area has higher priotity than polygon which has
                 # been saved in var "matched_polygon" and if so -> safe it.
                 if not matched_polygon:
-                    matched_polygon = area
-                if area['properties']['priority'] > matched_polygon['properties']['priority']:
-                    matched_polygon = area
+                    matched_polygon = poly_item
+                if area['properties']['priority'] > matched_polygon['area']['properties']['priority']:
+                    matched_polygon = poly_item
+        if area['geometry']['type'] == 'MultiPolygon':
+            print("got multipolygon",len(area['geometry']['coordinates']))
+            for polyg in area['geometry']['coordinates']:
+                poly_item  = Item(Polygon(polyg[0]), area)
+                poly_items.append(poly_item)
+                if poly_item.polygon.contains(point):
+                    # check if looped area has higher priotity than polygon which has
+                    # been saved in var "matched_polygon" and if so -> safe it.
+                    if not matched_polygon:
+                        matched_polygon = poly_item
+                    if area['properties']['priority'] > matched_polygon['area']['properties']['priority']:
+                        matched_polygon = poly_item
     if matched_polygon:
+        print(matched_polygon)
         return matched_polygon
+    else:
+        closest_item = min(poly_items, key=lambda item:item.distance_to_point(point))
+        print(closest_item)
+        distance = closest_item.distance_to_point(point)
+        print('closest poly')
+        print(distance)
+        print('asdads')
     return False
 
 def index(request):
@@ -45,6 +78,7 @@ def index(request):
     formdata = json.loads(request.body)['formdata']
     logger.debug('Formdata:',formdata)
     matched_polygon = validate_coordinates((formdata['locationInfo']['location']['lng'],formdata['locationInfo']['location']['lat']))
+    print(formdata['locationInfo']['location']['lng'],formdata['locationInfo']['location']['lat'])
     if not matched_polygon:
         logger.error('location_not_in_polygon',formdata)
         raise BadRequest({'error_message':'location_not_in_polygon'})
